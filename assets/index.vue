5b4caed07aa43de64b42f856ad9a16ce3ac04df8
user3954671843 <3572776139765801-user3954671843@noreply.coze.cn>
2026-03-11 23:07:41 +0800

    style: 调整分享按钮位置，靠近底部但不接触底部

--- assets/index.vue
+++ assets/index.vue
@@ -823,7 +823,7 @@ const hexToRgba = (hex, alpha) => {
   align-items: center;
   justify-content: center;
   gap: 16rpx;
-  margin: 32rpx;
+  margin: 16rpx 32rpx 64rpx 32rpx;
   padding: 24rpx;
   border-radius: 48rpx;
   background-color: v-bind('theme.backgroundTertiary');
